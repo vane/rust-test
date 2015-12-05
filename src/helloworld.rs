3f396a44df1cc1df53095eb7167da84c9ea41744
Michal Szczepanski <michal@vane.pl>
2015-12-05 20:13:44 +0100

    move philosophers / start count test

--- src/helloworld.rs
+++ src/helloworld.rs
@@ -1,44 +1,21 @@
 
 use std::thread;
 
-struct Philosopher {
-	name: String,
-}
-
-impl Philosopher {
-	fn new(name: &str) -> Philosopher {
-		Philosopher {
-			name: name.to_string(),
-		}
-	}
-
-	fn eat(&self) {
-		println!("{} is eating.", self.name);
-
-		thread::sleep_ms(1000);
-
-		println!("{} is done eating.", self.name);
-	}
-}
-
-
 fn main() {
-	println!("Siema");
-	let philosophers = vec![
-		Philosopher::new("Judith Butler"),
-		Philosopher::new("Gilles Deleuze"),
-		Philosopher::new("Karl Marx"),
-		Philosopher::new("Emma Goldman"),
-		Philosopher::new("Michel Foucault"),
-	];
 
-	let handles: Vec<_> = philosophers.into_iter().map(|p| {
-		thread::spawn(move || {
-			p.eat();
+	let handles: Vec<_> = (0..10).map(|_| {
+		thread::spawn(|| {
+			let mut x = 0;
+			for _ in (0..5_000_000) {
+				x += 1
+			}
+			x
 		})
 	}).collect();
 
 	for h in handles {
-		h.join().unwrap();
+		println!("Thread finished with count={}",
+			h.join().map_err(|_| "Could not join a thread!").unwrap());
 	}
+	println!("done!");
 }
